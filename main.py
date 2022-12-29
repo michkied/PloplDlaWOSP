56f01ad5f7c5e3523764abcd16a373705d015529
Blender <63952420+mbledek@users.noreply.github.com>
2022-12-29 13:27:48 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-# PloplDlaWOSP
-#
-# Made with Python 3.9 & ❤love❤ by Michał Kiedrzyński (michkied#6677)
-# 2021 - 2022
-#
-# Gramy do końca świata i jeden dzień dzień dłużej!
-
+from datetime import datetime
 import discord
 from discord.ext import commands
-from datetime import datetime
+from discord import app_commands
 
 import auction
 import verification
 
+token = ''
 intents = discord.Intents.all()
+
 bot = commands.Bot(command_prefix='?', intents=intents)
 bot.remove_command('help')
 
@@ -20,16 +16,16 @@ bot.remove_command('help')
 @bot.event
 async def on_ready():
     date = datetime.now()
-    print('Logged in as:')
-    print(bot.user.name)
+    print(f'We have logged in as {bot.user}')
+    print(bot.user.display_name)
     print(bot.user.id)
     print(date.strftime('%d.%m.%Y  %H:%M'))
-    print('by Michał Kiedrzyński')
-    print('------')
+    print('by Michał Kiedrzyński\n\n')
+    print("REMEMBER TO SEND A NEW POST MESSAGE")
+
+    await bot.add_cog(auction.Auction(bot))
+    await bot.add_cog(verification.Verification(bot))
 
 
-if __name__ == '__main__':
-    bot.add_cog(auction.Auction(bot))
-    bot.add_cog(verification.Verification(bot))
-
-    bot.run('TOKEN', reconnect=True)
+if __name__ == "__main__":
+    bot.run(token)
