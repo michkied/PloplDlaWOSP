a6d339d750400aac5014b618fd2005811dda751e
michkied <michkied@gmail.com>
2023-01-02 01:44:20 +0100

    Add /revert

--- bot/auction/commands.py
+++ bot/auction/commands.py
@@ -7,20 +7,38 @@ from ..config import *
 path = str(pathlib.Path(__file__).parent.absolute())
 
 
-def update(data):
-    with open(path+r'\\data.json', 'w+', encoding='UTF-8') as f:
-        f.write(json.dumps(data))
-    with open(path+r'\\price.txt', 'w+', encoding='UTF-8') as f:
-        f.write(str(data['price']) + 'zł')
-    with open(path+r'\\sum.txt', 'w+', encoding='UTF-8') as f:
-        f.write(str(data['sum']) + 'zł')
-
-
 class Auction(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        with open(path+'\\data.json', 'r+', encoding="UTF-8") as f:
+        with open(path + '\\data.json', 'r+', encoding="UTF-8") as f:
             self.data = json.loads(f.read())
+        with open(path + '\\history.json', 'r+', encoding="UTF-8") as f:
+            history = json.loads(f.read())
+            self.history = {int(price): history[price] for price in history}
+
+    async def get_prices(self, ctx: discord.AutocompleteContext):
+        return sorted(self.history.keys(), reverse=True)
+
+    def clear_history(self):
+        self.history = {}
+        with open(path + r"\\history.json", "w+") as f:
+            f.write('{}')
+
+    def update_files(self, data):
+        with open(path + r'\\data.json', 'w+', encoding='UTF-8') as f:
+            f.write(json.dumps(data))
+        with open(path + r'\\price.txt', 'w+', encoding='UTF-8') as f:
+            f.write(str(data['price']) + 'zł')
+        with open(path + r'\\sum.txt', 'w+', encoding='UTF-8') as f:
+            f.write(str(data['sum']) + 'zł')
+
+        if str(data['price']) in self.history:
+            self.history.pop(str(data['price']))
+        self.history[data['price']] = dict(
+            filter(lambda x: x[0] in ['highest_bidder', 'sum'], data.items())
+        )
+        with open(path + r'\\history.json', "w+", encoding='UTF-8') as f:
+            f.write(json.dumps(self.history, indent=4))
 
     @commands.Cog.listener()
     async def on_message(self, msg):
@@ -43,7 +61,12 @@ class Auction(commands.Cog):
                     logger.warning(f"{msg.author.display_name} próbował podbić cenę o {new_price - self.data['price']}")
                     return
 
-                if new_price < self.data['price'] + 2:
+                if self.data['highest_bidder']:
+                    diff = 2
+                else:
+                    diff = 0
+
+                if new_price < self.data['price'] + diff:
                     await msg.delete()
                     try:
                         await msg.author.send(':x: **Podana przez ciebie cena nie jest wyższa od poprzedniej o co najmniej 2zł**')
@@ -58,7 +81,8 @@ class Auction(commands.Cog):
                     self.data['sum'] = self.data['sum'] + new_price
                 self.data['price'] = new_price
                 self.data['highest_bidder'] = msg.author.id
-                await self.bot.loop.run_in_executor(None, update, self.data)
+
+                await self.bot.loop.run_in_executor(None, self.update_files, self.data)
                 await msg.channel.send(f'**{msg.author.mention} podbija cenę do `{new_price} zł`!**')
                 logger.info(f'{msg.author.display_name} podbija cenę do {new_price}')
 
@@ -91,10 +115,11 @@ class Auction(commands.Cog):
         self.data['running'] = True
         self.data['highest_bidder'] = 0
         self.data['starting_price'] = price
-        await self.bot.loop.run_in_executor(None, update, self.data)
+        await self.bot.loop.run_in_executor(None, self.clear_history)
+        await self.bot.loop.run_in_executor(None, self.update_files, self.data)
         # await ctx.channel.set_permissions(ctx.guild.default_role, overwrite=discord.PermissionOverwrite(send_messages=True, read_messages=True))
         await ctx.response.send_message(f':moneybag: **Licytacja `{name}` rozpoczęła się!**\nCena wywoławcza: `{price} zł`')
-        logger.info("Start licytacji: *"+ name + "* Cena:" + str(price))
+        logger.info("Start licytacji: *" + name + "* Cena:" + str(price))
 
     @commands.slash_command()
     async def end(self, ctx):
@@ -116,7 +141,7 @@ class Auction(commands.Cog):
             return
 
         self.data['running'] = False
-        await self.bot.loop.run_in_executor(None, update, self.data)
+        await self.bot.loop.run_in_executor(None, self.update_files, self.data)
         # await ctx.channel.set_permissions(ctx.guild.default_role, overwrite=discord.PermissionOverwrite(send_messages=False, read_messages=True))
         if self.data['highest_bidder'] != 0:
             await ctx.response.send_message(f"▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n:tada: **Licytacja zakończyła się!** :tada:\n"
@@ -127,3 +152,29 @@ class Auction(commands.Cog):
         else:
             await ctx.response.send_message(f"▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n**Licytacja zakończyła się!**\nNikt nie wylicytował przedmiotu")
             logger.info("Licytacja zakończyła się, nikt nie wylicytował przedmiotu")
+
+    @commands.slash_command()
+    async def revert(self, ctx,
+                     price: discord.Option(
+                         int, "Cena, do której cofnąć licytację",
+                         name="cena", autocomplete=get_prices
+                     )):
+
+        to_del = []
+        for bid in self.history:
+            if bid > price:
+                to_del.append(bid)
+        for bid in to_del:
+            self.history.pop(bid)
+
+        self.data['price'] = price
+        self.data['highest_bidder'] = self.history[price]['highest_bidder']
+        self.data['sum'] = self.history[price]['sum']
+        await self.bot.loop.run_in_executor(None, self.update_files, self.data)
+        if self.data['highest_bidder'] != 0:
+            text = f':warning: **Licytacja została cofnięta do wcześniejszego stanu**\n' \
+                   f'Aktualną kwotą jest `{price} zł` od **<@{self.data["highest_bidder"]}>**'
+        else:
+            text = f':warning: **Licytacja została cofnięta do kwoty wywoławczej - `{price} zł`**'
+
+        await ctx.response.send_message(text)
