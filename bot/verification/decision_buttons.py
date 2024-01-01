bac6147b5cf6785a9768c233e6c4098c4c1bb95e
michkied <michkied@gmail.com>
2024-01-02 00:33:22 +0100

    Add more info to logs

--- bot/verification/decision_buttons.py
+++ bot/verification/decision_buttons.py
@@ -13,17 +13,17 @@ class ApproveButton(discord.ui.Button):
         super().__init__(label="Zatwierdź", style=discord.enums.ButtonStyle.green, custom_id=f'{user_id}_approve')
 
     async def callback(self, interaction: discord.Interaction):
+        user = self.guild.get_member(self.user_id)
         if self.guild.get_role(VERIFIER_ROLE) not in interaction.user.roles:
             await interaction.response.send_message(':x: **Nie masz uprawnień by weryfikować użytkowników!**', ephemeral=True)
-            logger.warning("Nie masz uprawnień by weryfikować użytkowników (nie mam jak sprawdzić kim jesteś)")
+            logger.warning(f"{interaction.user.display_name} próbował zweryfikować {user.display_name}, ale nie ma uprawnień")
             return
 
-        user = self.guild.get_member(self.user_id)
         await user.add_roles(self.guild.get_role(VERIFIED_ROLES[self.user_type]))
         await user.remove_roles(self.guild.get_role(UNVERIFIED_ROLES[self.user_type]))
 
         description = f'{interaction.message.embeds[0].description}\n\n:white_check_mark: **Zweryfikowane przez {interaction.user.mention}** <t:{int(time.time())}>'
-        logger.info(f"Zatwierdził: {interaction.user.display_name}")
+        logger.info(f"Wniosek użytkownika {user.display_name} został zatwierdzony przez {interaction.user.display_name}")
 
         embed = discord.Embed(description=description, color=discord.Color.green())
         await interaction.message.edit(embed=embed, view=discord.ui.View())
@@ -44,16 +44,16 @@ class DenyButton(discord.ui.Button):
         super().__init__(label="Odrzuć", style=discord.enums.ButtonStyle.red, custom_id=f'{user_id}_deny')
 
     async def callback(self, interaction: discord.Interaction):
+        user = self.guild.get_member(self.user_id)
         if self.guild.get_role(VERIFIER_ROLE) not in interaction.user.roles:
             await interaction.response.send_message(':x: **Nie masz uprawnień by weryfikować użytkowników!**', ephemeral=True)
-            logger.warning("Nie masz uprawnień by weryfikować użytkowników (nie mam jak sprawdzić kim jesteś)")
+            logger.warning(f"{interaction.user.display_name} próbował zweryfikować {user.display_name}, ale nie ma uprawnień")
             return
 
-        user = self.guild.get_member(self.user_id)
         await user.remove_roles(self.guild.get_role(UNVERIFIED_ROLES[self.user_type]))
 
         description = f'{interaction.message.embeds[0].description}\n\n:x: **Odrzucone przez {interaction.user.mention}** <t:{int(time.time())}>'
-        logger.info(f"Odrzucił: {interaction.user.display_name}")
+        logger.info(f"Wniosek użytkownika {user.display_name} został odrzucony przez {interaction.user.display_name}")
 
         embed = discord.Embed(description=description, color=discord.Color.red())
         await interaction.message.edit(embed=embed, view=discord.ui.View())
